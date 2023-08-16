db8b60f81a535cbf9964a751d8cba62bc7785610
SARVAR RAHMONOV <tturkbola@gmail.com>
2023-08-16 14:55:34 +0500

    feat: login and register view added

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -3,10 +3,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view({'get': 'list'}), name='user-login'),
 ]
+
+
+
 
--- apps/common/views.py
+++ apps/common/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import UserSerializer, UserLoginSerializer
+from django.contrib.auth.models import User
+from apps.common.utils import get_tokens_for_user
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = get_tokens_for_user(user)
+        return Response(
+            {"user": UserSerializer(user, context=self.get_serializer_context()).data,
+             "token": token},
+            status=status.HTTP_201_CREATED,
+        )
+
+class UserLoginView(ModelViewSet):
+    serializer_class = UserLoginSerializer
+    def get_queryset(self):
+        return []
+    def post(self, request, *args, **kwargs):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+            if user:
+                token = get_tokens_for_user(user)
+                return Response({"token": token})
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
